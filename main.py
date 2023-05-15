bfa96cd88203631424d751d4e84c4a33fdd5cc57
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-05-15 13:39:12 +0100

    Got ther basic app set up

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ import tkinter
 import customtkinter
 from pytube import YouTube
 
+# System setting (This will adapt to the users system setting e.g light/dark mode)
+customtkinter.set_appearance_mode("System")
+
+# Sets the default color scheme to blue 
+customtkinter.set_default_color_theme("blue")
+
+# Creating the frame for the app
+app = customtkinter.CTk()
+app.geometry("720x480")
+app.title("Youtube Downloader")
+
