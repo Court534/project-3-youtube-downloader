f60bd81bffa249c7327695e56f2f84314942ef4e
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-05-15 14:40:34 +0100

    Added missing comments

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ from pytube import YouTube
 # Download function
 def startDownload():
     try:
+        # Gets the pasted link and tries to donwload the highest resolution possible
         ytLink = link.get()
         ytObject = YouTube(ytLink)
         video = ytObject.streams.get_highest_resolution()
-        
+        # Makes sure the title is white and appears once downloaded
         title.configure(text=ytObject.title, text_color="white")
         finishedLabel.configure(text="")
         video.download()
+        # Once the video is downloaded you get a success message in green
         finishedLabel.configure(text="Video Downloaded!", text_color="green")
     except:
+        # If the download fails you get an alert in red
         finishedLabel.configure(text="Download Error!", text_color="red")
         
 # System setting (This will adapt to the users system setting e.g light/dark mode)
